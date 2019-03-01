2ae2c27ee72727c76be9556f76a25e84755de64f
Ryan Gleason <rjg5404@psu.edu>
2019-03-01 05:44:09 -0500

    Profile form

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,14 @@
 import React, { Component } from "react";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
-import { Provider } from "react-redux";
-import store from "./store";
 import jwt_decode from "jwt-decode";
+import { Provider } from "react-redux";
+
+import store from "./store";
 import setAuthToken from "./utils/setAuthToken";
+
 import { setCurrentUser } from "./actions/authActions";
 import { logoutUser } from "./actions/authActions";
+import { clearCurrentProfile } from "./actions/profileActions";
 
 import Navbar from "./components/layout/Navbar";
 import Footer from "./components/layout/Footer";
@@ -13,10 +16,10 @@ import Landing from "./components/layout/Landing";
 import Register from "./components/auth/Register";
 import Login from "./components/auth/Login";
 import Dashboard from "./components/dashboard/Dashboard";
-import "./App.css";
-import { clearCurrentProfile } from "./actions/profileActions";
-
 import PrivateRoute from "./components/common/PrivateRoute";
+import CreateProfile from "./components/create-profile/CreateProfile";
+
+import "./App.css";
 
 if (localStorage.jwtToken) {
   // Set auth token header auth
@@ -56,6 +59,13 @@ class App extends Component {
 
               <Switch>
                 <PrivateRoute exact path="/dashboard" component={Dashboard} />
+              </Switch>
+              <Switch>
+                <PrivateRoute
+                  exact
+                  path="/create-profile"
+                  component={CreateProfile}
+                />
               </Switch>
             </div>
             <Footer />
