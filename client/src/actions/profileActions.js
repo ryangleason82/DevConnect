409adf25f828c37884fa0d63838144a9e6d12671
Ryan Gleason <rjg5404@psu.edu>
2019-03-05 06:49:04 -0500

    Each individual profile

--- client/src/actions/profileActions.js
+++ client/src/actions/profileActions.js
@@ -3,7 +3,7 @@ import axios from "axios";
 import {
   GET_PROFILE,
   PROFILE_LOADING,
-  //GET_ERRORS,
+  GET_PROFILES,
   CLEAR_CURRENT_PROFILE,
   GET_ERRORS,
   SET_CURRENT_USER
@@ -24,6 +24,25 @@ export const getCurrentProfile = () => dispatch => {
       dispatch({
         type: GET_PROFILE,
         payload: {}
+      })
+    );
+};
+
+// Get profile by handle
+export const getProfileByHandle = handle => dispatch => {
+  dispatch(setProfileLoading());
+  axios
+    .get(`/api/profile/handle/${handle}`)
+    .then(res =>
+      dispatch({
+        type: GET_PROFILE,
+        payload: res.data
+      })
+    )
+    .catch(err =>
+      dispatch({
+        type: GET_PROFILE,
+        payload: null
       })
     );
 };
@@ -90,6 +109,26 @@ export const deleteExperience = id => dispatch => {
       dispatch({
         type: GET_ERRORS,
         payload: err.response.data
+      })
+    );
+};
+
+// Get all profiles
+
+export const getProfiles = () => dispatch => {
+  dispatch(setProfileLoading());
+  axios
+    .get("api/profile/all")
+    .then(res =>
+      dispatch({
+        type: GET_PROFILES,
+        payload: res.data
+      })
+    )
+    .catch(err =>
+      dispatch({
+        type: GET_PROFILES,
+        payload: null
       })
     );
 };
