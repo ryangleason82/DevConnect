d761c4773271bf6caee8398b144fd05187063954
Ryan Gleason <rjg5404@psu.edu>
2019-03-08 09:21:10 -0500

    Prepared for deploy

--- server.js
+++ server.js
@@ -36,12 +36,11 @@ app.use("/api/posts", posts);
 
 // Server static assets if in production
 if (process.env.NODE_ENV === "production") {
-	console.log("TRYING TO THE DB");
 	// Set static folder
 	app.use(express.static("client/build"));
+
 	app.get("*", (req, res) => {
 		res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
-		console.log("CONNECTED TO THE DB");
 	});
 }
 
